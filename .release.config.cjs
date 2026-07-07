f325b3a5170039643157c240cb0825fe0d093248
Varun Darshan <shrivarun63@gmail.com>
2026-07-07 23:16:47 +0530

    Refactor code structure for improved readability and maintainability

---
+++ .release.config.cjs
@@ -0,0 +1,26 @@
+modeule.exports = {
+    branches: ["main"],
+
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "CHANGELOG.md"
+            }
+        ],
+
+        "@semantic-release/github",
+
+        [
+            "@semantic-release/git",
+            {
+                assets: ["CHHANGELOG.md"],
+                message: 
+                    "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ]
+    ]
+};
